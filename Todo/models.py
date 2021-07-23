a0246d76f4d57d32db211ed1a8731c87583aee42
Raphael Sparenberg <raphaelsparenberg@kateinnovations.com>
2021-07-23 09:02:04 +0200

    Fixed the api backend connection

--- Todo/models.py
+++ Todo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=350)
+    description = models.TextField(default="")
     completed = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
