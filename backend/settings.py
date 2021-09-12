4ace5e109cd64dccb16ea2a0447b19856e10bbc8
Raphael Sparenberg <raphaelsparenberg@kateinnovations.com>
2021-09-12 13:44:12 +0200

    Edited allowed host and removed redundant requirements

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", config("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
