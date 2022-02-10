58f021c4e5d0bcdb8ed180e528046d14d7f7f88f
RaphaelSP <rsparenberg1992@gmail.com>
2022-02-10 21:32:53 +0100

    fix link to api exanmples

--- api_backend/urls.py
+++ api_backend/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name='api_overview'),
-    path('geo-api', views.geo_api_get_ip, name='geo_api_get_ip')
+    path('geo-api/get-ip', views.geo_api_get_ip, name='geo_api_get_ip'),
+    path('/get-ip', views.geo_api_get_ip, name='geo_api_get_ip')
 ]
