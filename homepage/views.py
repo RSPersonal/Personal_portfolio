4401d7b0409d675aaa7d233dd8d2a20912677447
Raphael Sparenberg <raphaelsparenberg@kateinnovations.com>
2022-02-24 20:16:58 +0100

    cleaned up homepage

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,12 @@ def home_page(request):
     """
     Function for homepage render
     """
-    return render(request, 'home_page.html')
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    context = {
+        'num_visits' : num_visits
+    }
+    return render(request, 'home_page.html', context)
 
 
 def about_me(request):
