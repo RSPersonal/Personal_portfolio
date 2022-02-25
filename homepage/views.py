3c6b751458104d8b20d5211e1fab53f0fc29cfff
Raphael Sparenberg <raphaelsparenberg@kateinnovations.com>
2022-02-25 18:20:18 +0100

    Visitor Counter with database functionality

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import VisitorCount
 
 
 # Create your views here.
@@ -6,10 +7,14 @@ def home_page(request):
     """
     Function for homepage render
     """
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+    num_visit = VisitorCount.objects.all()[0]
+    num_visit.visitor_count += 1
+    num_visit.save()
+
+    num_current_visits = VisitorCount.objects.get(pk=1)
+
     context = {
-        'num_visits' : num_visits
+        'num_visits': num_current_visits
     }
     return render(request, 'home_page.html', context)
 
