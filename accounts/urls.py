bb0180c9d390c7a69b7d12d858bda7c5a450ad4b
RaphaelSP <rsparenberg1992@gmail.com>
2022-03-08 20:37:21 +0100

    Login page and login functionaliy

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'))
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html', next='registration/login_valid.html'))
 ]
