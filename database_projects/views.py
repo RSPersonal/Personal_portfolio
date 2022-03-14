85ba02bb2df5c875354594ccdce8b27e0bb57820
RaphaelSP <rsparenberg1992@gmail.com>
2022-03-14 08:46:50 +0100

    Build view function to query database

--- database_projects/views.py
+++ database_projects/views.py
@@ -37,12 +37,23 @@ def portfolio_detail(request, pk):
         'position_form': position_form,
         'portfolio_name': portfolio_name
     }
+    # See if there are any active positions
     if Positions.objects.filter(pk=pk).exists():
         positions = Positions.objects.get(portfolio_id=pk)
         context['position'] = positions
     else:
         context['active_positions'] = False
 
+    # Adding new positions to database
     if request.method == 'POST':
-        pass
+        form = PositionForm(request.POST)
+        if form.is_valid():
+            stock_ticker = form.cleaned_data['ticker_name']
+            buy_price = form.cleaned_data['buy_price']
+            quantity = form.cleaned_data['quantity']
+            market = form.cleaned_data['market']
+
+            new_stock_entry = Positions(ticker_name=stock_ticker, buy_price=buy_price, quantity=quantity, market=market)
+            new_stock_entry.save()
+
     return render(request, 'database-projects/portfolio_detail.html', context=context)
