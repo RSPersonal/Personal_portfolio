8cdae2dd7760e74b3bbcf35d809d0d14244b61bb
RaphaelSP <rsparenberg1992@gmail.com>
2022-04-14 08:08:33 +0200

    better releaser version function

--- releaser.py
+++ releaser.py
@@ -1,4 +1,4 @@
-
+old_history_content = ''
 
 with open('version.txt', 'r') as file:
     print(f"Current version number is: {file.read()}")
@@ -10,3 +10,14 @@ with open('version.txt', 'w') as file:
     file.write(new_version_number)
     file.close()
 
+with open('history.txt', 'r') as file:
+    old_history_content = file.read()
+    file.close()
+
+new_history_entry = input('Enter changelog entry: ')
+
+with open('history.txt', 'w') as file:
+    file.write(f"v{new_version_number}/({new_history_entry})")
+    file.write('\n')
+    file.write(old_history_content)
+    file.close()
