a4f5aed0cdb4775b94ceee54677820dd2cb226a2
RaphaelSP <rsparenberg1992@gmail.com>
2022-04-14 07:34:50 +0200

    linter

--- releaser.py
+++ releaser.py
@@ -1,5 +1,12 @@
-import os
+
 
 with open('version.txt', 'r') as file:
-    file.read()
+    print(f"Current version number is: {file.read()}")
+    file.close()
+
+new_version_number = input('Enter new version number: ')
+
+with open('version.txt', 'w') as file:
+    file.write(new_version_number)
+    file.close()
 
