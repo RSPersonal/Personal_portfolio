4f232135d4da09bb19deb6cf1b022eba37b58c9f
RaphaelSP <rsparenberg1992@gmail.com>
2022-05-12 08:38:52 +0200

    added property model

--- website_projects/models.py
+++ website_projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class PropertyModel(models.Model):
+    street = models.CharField(max_length=50)
+    housenumber = models.IntegerField(blank=True, null=True)
+    housenumber_add = models.CharField(max_length=15)
+    zipcode = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+    municipality = models.CharField(max_length=60)
+    province = models.CharField(max_length=60)
+    ask_price = models.FloatField()
+    construction_year = models.IntegerField(blank=True, null=True)
+    amount_rooms = models.IntegerField(default=0, null=True)
+    description = models.TextField()
