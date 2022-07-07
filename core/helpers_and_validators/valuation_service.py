4501232bb0f1c5b10ecefc69a5379888c8196de9
Raphael <raphaelsparenberg@kateinnovations.com>
2022-07-07 08:12:03 +0200

    added nla range to query

--- core/helpers_and_validators/valuation_service.py
+++ core/helpers_and_validators/valuation_service.py
@@ -1,11 +1,16 @@
 from website_projects.models import ScrapyPropertyModel
 from core.helpers_and_validators.calculator import calculate_mean_price
 
-def get_properties_within_postal_code_range_and_nla_range(postal_code_range: list, nla_range: list):
+
+def get_properties_within_postal_code_range_and_nla_range(postal_code_range: list, nla_range: int):
     """
-    @param postal_code_range:
-    @return:
+    @param nla_range: int
+    @param postal_code_range: list
+    @return: list of queried properties
     """
+    nla_lower_range = nla_range - (float(nla_range) * 0.10)
+    nla_higher_range = nla_range + (float(nla_range) * 0.10)
+    print(nla_lower_range, nla_higher_range)
     properties = ScrapyPropertyModel.objects.filter(zipcode__range=(postal_code_range[0], postal_code_range[-1]))
     return properties
 
@@ -24,4 +29,4 @@ def get_mean_property_price(properties: list):
         count_properties += 1
         price_list.append(scraped_property.ask_price)
     mean_price = calculate_mean_price(price_list)
-    return mean_price
+    return round(mean_price, 0)
