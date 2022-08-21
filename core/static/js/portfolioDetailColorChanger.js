d2935ce53ed1e640e08f56f50da725f5aec51235
Raphael <raphaelsparenberg@kateinnovations.com>
2022-08-21 11:09:11 +0200

    dynamic coloring of dailyreturn and overalReturn in portfolioDetial

--- core/static/js/portfolioDetailColorChanger.js
+++ core/static/js/portfolioDetailColorChanger.js
@@ -1,14 +1,22 @@
-const portfolioTotalProfit = JSON.parse(document.getElementById("portfolioOveralReturn").textContent);
+const valuesToCheck = {};
+valuesToCheck['mainElements'] = [];
+const portfolioOveralReturn = {};
+portfolioOveralReturn['item'] = 'portfolioOveralReturn';
+portfolioOveralReturn['divID'] = 'overalReturnDiv';
+portfolioOveralReturn['value'] = JSON.parse(document.getElementById("portfolioOveralReturn").textContent);
+valuesToCheck.mainElements.push(portfolioOveralReturn);
 
-var idArray = ['portfolioOveralReturn'];
+const dailyReturn = {};
+dailyReturn['item'] = 'dailyReturn';
+dailyReturn['divID'] = 'dailyReturnDiv';
+// TODO: Daily return still needs to be calculated and prepared for this value
+dailyReturn['value'] = JSON.parse(document.getElementById("portfolioDailyReturn").textContent);
+valuesToCheck.mainElements.push(dailyReturn);
 
-idArray.forEach(item => {
-    console.log(item)
-
-    // const itemValue = JSON.parse(document.getElementById(`$item`).textContent);
-    // if (itemValue < 0) {
-        // const element = document.getElementById(`"$item"`);
-        // const elementText = document.getElementById("overalReturnDivText");
-        // element.style.backgroundColor='red';
-    // }
+valuesToCheck.mainElements.forEach(item => {
+    let itemValue = item.value;
+    if (itemValue < 0) {
+        const element = document.getElementById(item.divID);
+        element.style.backgroundColor='red';
+    }
 })
