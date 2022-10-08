39eed3bd417ddba465d669bf5dcd8ecdc1a4b336
RaphaelSP <rsparenberg1992@gmail.com>
2022-10-08 10:53:54 +0200

    refactor of portfoliodetailDailyreturn script, added api call to get latest price, correct checking of negative values for red or green color change

--- core/static/js/portfolioDetailColorChanger.js
+++ core/static/js/portfolioDetailColorChanger.js
@@ -5,42 +5,46 @@ const portfolioID = JSON.parse(document.getElementById("portfolioID").textConten
 const apiHost = JSON.parse(document.getElementById("apiHost").textContent);
 console.log(apiHost);
 
-let fetchedLatestPrice;
 
-fetch(`${apiHost}/api/v1/daily-return/${portfolioID}`)
+
+fetch(`${apiHost}api/v1/daily-return/${portfolioID}`)
     .then((response) => response.json())
     .then((data) => {
-        fetchedLatestPrice = data.data[0].last_price;
-        if (fetchedLatestPrice) {
-            document.getElementById("dailyReturnItem").innerHTML = `€ ${fetchedLatestPrice.toLocaleString()}`;
+        let fetchedLatestPrice;
+        if (data.data[0]) {
+            fetchedLatestPrice = data.data[0].last_price;
+            console.log(fetchedLatestPrice);
+        } else {
+            fetchedLatestPrice = 0;
         }
+        document.getElementById("dailyReturnItem").innerHTML = `€ ${fetchedLatestPrice.toLocaleString()}`;
+        
+        // Portfolio overal return
+        valuesToCheck['mainElements'] = [];
+        const portfolioOveralReturn = {};
+        portfolioOveralReturn['item'] = 'portfolioOveralReturn';
+        portfolioOveralReturn['divID'] = 'overalReturnDiv';
+        portfolioOveralReturn['value'] = JSON.parse(document.getElementById("portfolioOveralReturn").textContent);
+        valuesToCheck.mainElements.push(portfolioOveralReturn);
+
+        // Portfolio daily return
+        const dailyReturn = {};
+        dailyReturn['item'] = 'dailyReturn';
+        dailyReturn['divID'] = 'dailyReturnDiv';
+        dailyReturn['value'] = fetchedLatestPrice;
+        valuesToCheck.mainElements.push(dailyReturn);
+
+        valuesToCheck.mainElements.forEach(item => {
+            let itemValue = parseFloat(item.value);
+            console.log(itemValue);
+            if (itemValue < 0) {
+                const element = document.getElementById(item.divID);
+                console.log(element, itemValue, itemValue < 0);
+                element.style.backgroundColor = 'red';
+                element.style.borderRadius = "5px";
+            }
+        })
     })
     .catch((error) => {
         console.warn(error);
     });
-
-// Portfolio overal return
-valuesToCheck['mainElements'] = [];
-const portfolioOveralReturn = {};
-portfolioOveralReturn['item'] = 'portfolioOveralReturn';
-portfolioOveralReturn['divID'] = 'overalReturnDiv';
-portfolioOveralReturn['value'] = JSON.parse(document.getElementById("portfolioOveralReturn").textContent);
-valuesToCheck.mainElements.push(portfolioOveralReturn);
-
-// Portfolio daily return
-// TODO daily return div is to slow from api call which results in async checking of negative value
-// Meaning the call is to slow and the value gets checked to fast for positive or negative
-const dailyReturn = {};
-dailyReturn['item'] = 'dailyReturn';
-dailyReturn['divID'] = 'dailyReturnDiv';
-dailyReturn['value'] = JSON.parse(document.getElementById("portfolioDailyReturn").textContent);
-valuesToCheck.mainElements.push(dailyReturn);
-
-valuesToCheck.mainElements.forEach(item => {
-    let itemValue = item.value;
-    if (itemValue < 0) {
-        const element = document.getElementById(item.divID);
-        element.style.backgroundColor='red';
-        element.style.borderRadius= "5px";
-    }
-})
