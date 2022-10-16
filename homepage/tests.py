1b53c8494fd5be404073798960db2dfc815a3b65
RaphaelSP <rsparenberg1992@gmail.com>
2022-10-16 10:50:38 +0200

    testing main website

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test import Client
+from .models import VisitorCount
 
-# Create your tests here.
+
+class TestClient(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        VisitorCount.objects.create(visitor_count=1)
+
+    def test_main_homepage_en(self):
+        homepage = self.client.get('/')
+        self.assertEqual(homepage.status_code, 200)
+
+    def test_contact_page(self):
+        homepage = self.client.get('/contact')
+        self.assertEqual(homepage.status_code, 200)
+
